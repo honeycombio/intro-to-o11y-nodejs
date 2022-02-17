339ef9321347fd7f7aada94a3ca775f6d5d38031
Jessica Kerr <jessitron@gmail.com>
2022-02-17 15:23:56 -0600

    Use a message if passed in

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 # "Deploy"... this doesn't really do anything to push to production
 # but it does make a marker in honeycomb _representing_ a deploy.
 
+message=$1
+
 source ./.env
 
 sha=$(git rev-parse HEAD)
@@ -10,8 +12,10 @@ sha=$(git rev-parse HEAD)
 echo "Dataset: $HONEYCOMB_DATASET"
 echo "Current SHA: " $sha
 
+defaultMessage="commit $sha"
+
 # this creates a deploy marker in Honeycomb
 
 curl https://api.honeycomb.io/1/markers/$HONEYCOMB_DATASET -X POST  \
     -H "X-Honeycomb-Team: $HONEYCOMB_API_KEY"  \
-    -d "{\"message\":\"commit $sha\", \"type\":\"deploy\"}"
+    -d "{\"message\":\"${message:-$defaultMessage}\", \"type\":\"deploy\"}"
