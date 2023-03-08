3a8779cea055deec3694f4626b4a558b7a5d6ad5
Jessica Kerr <jessitron@honeycomb.io>
2023-03-08 10:58:46 -0600

    Update to use the SDK, as recommended in the docs
    also, this makes the OTEL_SERVICE_NAME env var work

--- src/tracing.js
+++ src/tracing.js
@@ -1,5 +1,5 @@
 // tracing.js
-'use strict';
+"use strict";
 
 /**
  * This implementation deliberately uses a harder initialization
@@ -27,6 +27,7 @@ const { DiagConsoleLogger, DiagLogLevel, diag } = require("@opentelemetry/api");
 
 // import otel dependencies
 const opentelemetry = require("@opentelemetry/api");
+const { NodeSDK } = require("@opentelemetry/sdk-node");
 const { ConsoleLogger } = require("@opentelemetry/core");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const {
@@ -49,47 +50,39 @@ const {
 } = require("@opentelemetry/semantic-conventions");
 
 // set log level to DEBUG for a lot of output
+// setting this to INFO means you'll see connection errors
 opentelemetry.diag.setLogger(
   new opentelemetry.DiagConsoleLogger(),
   opentelemetry.DiagLogLevel.INFO
 );
 
+// set up gRPC. This is faster at runtime; HTTP is easier to setup
+
 const apikey = process.env.HONEYCOMB_API_KEY;
-const serviceName = process.env.OTEL_SERVICE_NAME || "sequence-of-numbers";
+const metadata = new grpc.Metadata();
+metadata.set("x-honeycomb-team", apikey);
+
+const sendBatchesToHoneycomb = new BatchSpanProcessor(
+  new OTLPTraceExporter({
+    url: "grpc://api.honeycomb.io:443/",
+    credentials: grpc.credentials.createSsl(),
+    metadata,
+  }),
+  {
+    scheduledDelayMillis: 500,
+    maxQueueSize: 16000,
+    maxExportBatchSize: 1000,
+  }
+);
+
+const sdk = new NodeSDK({
+  spanProcessor: sendBatchesToHoneycomb,
+  instrumentations: [new HttpInstrumentation(), new ExpressInstrumentation()],
+});
+
+sdk.start();
+
+const serviceName = process.env.OTEL_SERVICE_NAME || "unknown_service";
 console.log(
   `Exporting to Honeycomb with APIKEY <${apikey}> and service name ${serviceName}`
 );
-
-const provider = new NodeTracerProvider({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-  }),
-});
-const metadata = new grpc.Metadata();
-metadata.set("x-honeycomb-team", apikey);
-const creds = grpc.credentials.createSsl();
-provider.addSpanProcessor(
-  new BatchSpanProcessor(
-    new OTLPTraceExporter({
-      url: "grpc://api.honeycomb.io:443/",
-      credentials: creds,
-      metadata,
-    }),
-    {
-      scheduledDelayMillis: 500,
-      maxQueueSize: 16000,
-      maxExportBatchSize: 1000,
-    }
-  )
-);
-
-// uncomment this to see traces in stdout
-// provider.addSpanProcessor(new BatchSpanProcessor(new ConsoleSpanExporter()));
-
-provider.register();
-
-// turn on autoinstrumentation for traces you're likely to want
-registerInstrumentations({
-  tracerProvider: provider,
-  instrumentations: [new HttpInstrumentation(), new ExpressInstrumentation()],
-});
